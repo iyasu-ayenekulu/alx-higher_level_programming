5cdea937eb532eeb77135aabf66848386388ff5b
iyasu-ayenekulu <iyasuayenekulu3@gmail.com>
2022-12-05 23:49:19 -0800

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
-
-    for number in my_list:
-        if (number % 2) == 0:
-            new_list.append(True)
+    if not my_list:
+        return my_list
+    new_list = list(my_list)
+    for index in new_list:
+        if index % 2 == 0:
+            new_list[index] = True
         else:
-            new_list.append(False)
-            return(new_list)
+            new_list[index] = False
+            return new_list
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        my_tuple = (0, None)
-    else:
-        my_tuple = (len(sentence), sentence[:1])
-        return(my_tuple)
+    if not sentence:
+        sentence = None
+        if sentence:
+            sen_len = len(sentence)
+        else:
+            sen_len = 0
+            return (sen_len, sentence if not sentence else sentence[:1])
