bea6010517f3f4d117de64eb4eb20e775685c02d
josh <iyasuayenekulu3@gmail.com>
2022-12-19 08:53:26 -0800

    Update 3-safe_print_division.py
    done

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
-i
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    res = 0
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+        return res
