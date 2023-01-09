5df62fcafba3fcf2d510b422bbcd89f360d9546e
josh <iyasuayenekulu3@gmail.com>
2023-01-09 02:32:21 -0800

    Update 4-inherits_from.py
    done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
-i
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    if isinstance(obj, a_class) and \
+       issubclass(a_class, obj.__class__) is False:
+        return True
+
+    return False
