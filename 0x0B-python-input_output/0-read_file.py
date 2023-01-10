9e6b4a5fa29ce73f9e652e6387e8e08017e71879
josh <iyasuayenekulu3@gmail.com>
2023-01-10 01:04:00 -0800

    Update 0-read_file.py
    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,11 @@
-i
+#!/usr/bin/python3
+"""read_file
+"""
+
+
+def read_file(filename=""):
+    """Takes in str filename to read it
+    """
+
+    with open(filename, encoding="utf-8") as readFile:
+        print(readFile.read(), end='')
