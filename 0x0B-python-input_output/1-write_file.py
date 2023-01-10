5dfeece99bc354641a0c88e236918f877a906755
josh <iyasuayenekulu3@gmail.com>
2023-01-10 01:06:25 -0800

    Update 1-write_file.py
    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,12 @@
-i
+#!/usr/bin/python3
+"""write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """Takes str filename to read, and str text to write to
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as writeFile:
+        writeFile.write(text)
+        return len(text)
