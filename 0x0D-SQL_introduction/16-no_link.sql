acb3cb85c55fed3bc9357994cfdf16fa372d8e93
josh <iyasuayenekulu3@gmail.com>
2023-02-14 11:28:39 -0800

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,1 +1,7 @@
-i
+-- Lists all records of the table `second_table`
+-- of the database `hbtn_0c_0` in MySQL Server.
+
+SELECT score, name
+FROM second_table
+WHERE `name` IS NOT NULL
+ORDER BY score DESC;
