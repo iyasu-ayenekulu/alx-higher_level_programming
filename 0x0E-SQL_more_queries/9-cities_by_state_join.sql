4724ad8189aafbb3bd0867474ea364a21b4f6d85
josh <iyasuayenekulu3@gmail.com>
2023-02-15 07:46:29 -0800

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,5 @@
-i
+-- Lists all cities with their state contained in the
+-- database `hbtn_0d_usa`.
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
